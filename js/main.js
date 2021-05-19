de03d1ced44b5c4a7a9b4e22c03ec79ece38dff5
Simren Kshetrapal <simrenk@uchicago.edu>
2021-05-18 19:22:06 -0500

    Testing deployment

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 function setup() {
   createCanvas(innerWidth, innerHeight)
-  background(0)
+  background(255)
   let x = width / 2
   let y = height / 2
   let r = width / 4
+  strokeWeight(10)
+  stroke(255, 0, 0)
+  fill(255, 0, 255, 100)
   ellipse(x, y, r)
 }
