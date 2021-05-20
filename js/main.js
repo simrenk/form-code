773beb08c36ec1f3a4f167c48f1a3465134ec53a
Simren Kshetrapal <simrenk@uchicago.edu>
2021-05-19 22:39:38 -0500

    This is definitely the cooler version

--- js/main.js
+++ js/main.js
@@ -1,100 +1,70 @@
-// let d = 75
-// let total = 200
-// let xs = []
-// let ys = []
-// let dxs = []
-// let dys = []
+let d = 100
+let total = 100
+let xs = []
+let ys = []
+let dxs = []
+let dys = []
 
 
 
 function setup() {
-  createCanvas(innerWidth, innerHeight)
-  background(0)
+  createCanvas(innerWidth, innerHeight, WEBGL)
 
-  // for (let i = 0; i < total; i++) {
-  //   xs.push(random(0, width))
-  //   ys.push(innerHeight/2)
-  //   dxs.push(random(-2,2))
-  //   dys.push(random(-2,2))
-  // }
+  for (let i = 0; i < total; i++) {
+    xs.push(random(0, width))
+    ys.push(random(0, height))
+    dxs.push(random(-1,3))
+    dys.push(random(-3,1))
+  }
 }
 
-// // function drawOverlapsEllipse(x, y) {
-// //   let r = random(0, 255)
-// //   let g = random(0, 255)
-// //   let b = random(0, 255)
-// //   stroke(r,g,b, 100)
-// //   for (let i = 0; i < total; i++) {
-// //     let distance = dist(x, y, xs[i], ys[i])
-// //     if (distance < d && distance !=0) {
-// //       // fill(r, g, b, 100)
-// //       ellipse(x, y, xs[i], ys[i])
-// //     }
-// //   }
-// // }
+function drawOverlaps(x, y) {
+  let r = random(0, 255)
+  let g = random(0, 255)
+  let b = random(0, 255)
+  stroke(r,g,b, 100)
+  for (let i = 0; i < total; i++) {
+    let distance = dist(x, y, xs[i], ys[i])
+    if (distance < d && distance != 0) {
+      ellipsoid(x, y, xs[i])
+    }
+  }
+}
 
 // function drawOverlapsLines(x, y) {
 //   let r = random(0, 255)
 //   let g = random(0, 255)
 //   let b = random(0, 255)
-//   stroke(r,g,b, 20)
+//   stroke(r,g,b, 100)
 //   for (let i = 0; i < total; i++) {
 //     let distance = dist(x, y, xs[i], ys[i])
-//     if (distance < d && distance !=0) {
-//       line(x, y, xs[i], ys[i])
+//     if (distance < d && distance != 0) {
+//       lines(x, y, xs[i], ys[i])
 //     }
 //   }
 // }
 
-
-function mouseMoved () {
-  // let x = random(0, width)
-  // let y = random(0, height)
-  let x = mouseX
-  let y = height/16
-  let d = mouseY
-
-  let r = random(0, 255)
-  let g = random(0, 255)
-  let b = random(0, 255)
-  
-  strokeWeight(5)
-  stroke(255, 0, 0)
-  fill(x, g, d, 100)
-  square(x, y, d)
-  drawOverlapsLines(x, y)
-
+function draw() {
+  for (let i = 0; i < total; i++) {
+    xs[i] += dxs[i]
+    ys[i] += dys[i]
+    drawOverlaps(xs[i], ys[i])
+  }
 }
+// function mouseMoved () {
+//   // let x = random(0, width)
+//   // let y = random(0, height)
+//   let x = mouseX
+//   let y = mouseY
+//   let d = random(100, 200)
 
-// // function mouseIsPressed () {
-
-// //   // let x = random(0, width)
-// //   // let y = random(0, height)
-// //   let x = mouseX
-// //   let y = height/16
-// //   let d = mouseY
-
-// //   let r = random(0, 255)
-// //   let g = random(0, 255)
-// //   let b = random(0, 255)
+//   let r = random(0, 255)
+//   let g = random(0, 255)
+//   let b = random(0, 255)
   
-// //   strokeWeight(5)
-// //   stroke(255, 0, 0)
-// //   fill(x, g, d, 100)
-// //   ellipse(x, y, d)
-
-
-// // }
-
-// function draw() {
-//   background(0)
-//   for (let i = 0; i < total; i++) {
-//     // xs[i] += dxs[i]
-//     ys[i] += dys[i]
-//     // drawOverlapsEllipse(xs[i], ys[i])
-//     if (ys[i] > width || ys[i] < 0) dys[i] = -dys[i] 
-//     drawOverlapsLines(xs[i], ys[i])
-//     // drawOverlapsEllipse(xs[i], ys[i])
-
-//   }
+//   strokeWeight(5)
+//   stroke(255, 0, 0)
+//   fill(r, g, b, 100)
+//   box(x, y, d)
 // }
+
